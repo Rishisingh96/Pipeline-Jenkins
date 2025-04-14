00d57c42f13ba29b8ef03aeb0489d4dfdd185d76
rishisingh <rishicoding9838@gmail.com>
2025-04-14 11:52:00 +0530

    Build Test

--- Test.java
+++ Test.java
@@ -3,5 +3,9 @@ class Test{
         System.out.println("Hello, World!");
        
         System.out.println("Hello, Rishi Singh");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Sum of a and b is: " + c);
     }
 }
